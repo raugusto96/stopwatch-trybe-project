639c22593e6260242f6b2e81bbe6abf342254e8c
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-08-06 12:13:02 -0300

    Finish functionalities

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/stopwatch.css'
 import * as timeFunctions from '../functions/time';
+import { Link } from 'react-router-dom';
+
 import {
   MdAlarmOff,
   MdAlarmOn,
@@ -16,58 +18,125 @@ export default class StopWatch extends React.Component {
   constructor() {
     super();
 
-    this.state = {
+    this.initialState = {
       time: 0,
       temporaryTime: '',
       minutes: 0,
       seconds: 0,
       startCountdownDisabled: true,
       showTimer: false,
-      showButtons: true,
-    };
+      countdownEnd: false,
+    }
+
+    this.state = this.initialState;
+
+    this.countdownTimeout = null;
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.time !== this.state.time) {
+      this.setMinutesAndSeconds();
+      if (this.state.time > 0) this.startCountdown();
+      else if (!this.state.countdownCanceled) {
+        this.setState({ showTimer: false, countdownEnd: true });
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.countdownTimeout);
   }
 
   handleInputTime = ({ target }) => {
     const isValid = timeFunctions.validadeInputTime(target.value);
     this.setState({
       startCountdownDisabled: !isValid,
+      temporaryTime: target.value,
     });
   }
 
-  handleFastTimer = (event) => {
-    event.preventDefault();
-    this.setState((prevState) => ({
-      minutes: 3,
-      showTimer: !prevState.showTimer,
-    }))
+  setMinutesAndSeconds = () => {
+    const { time } = this.state;
+    const { convertTimeToMinutesAndSeconds } = timeFunctions;
+    const { minutes, seconds } = convertTimeToMinutesAndSeconds(time);
 
+    this.setState({ minutes, seconds });
   }
 
-  handleBreakTimer = (event) => {
+  customInterval = (event) => {
     event.preventDefault();
-  }
-  
-  handleGeoGuessrTimer = (event) => {
-    event.preventDefault();
-  }
-  
-  handleRandomTimer = (event) => {
-    event.preventDefault();
-  }
-  
-  setStartCountdown = (event) => {
-    event.preventDefault();
+    const { convertCustomTimeToSeconds } = timeFunctions;
+    let { temporaryTime } = this.state;
+
+    const time = convertCustomTimeToSeconds(temporaryTime);
+
     this.setState({
-      showButtons: false,
+      time,
+      temporaryTime: '',
+      startCountdownDisabled: true,
+      showTimer: true,
+    });
+  }
+
+  startCountdown = () => {
+    const { time } = this.state;
+    this.countdownTimeout = setTimeout(() => {
+      this.setState({
+        ...this.initialState,
+        time: time -1,
+        showTimer: true,
+        countdownEnd: false,
+      })
+    }, 1000);
+  }
+
+  cancelCountdown = () => {
+    clearTimeout(this.countdownTimeout);
+    this.setState({ time: 0, showTimer: false, countdownCanceled: true });
+  }
+
+  goBack = () => {
+    this.setState({ showTimer: false, countdownEnd: true });
+  }
+
+  setFastTimer = () => {
+    this.setState({
+      temporaryTime: '3m',
+    })
+  }
+
+  setCoffeeTimer = () => {
+    this.setState({
+      temporaryTime: '5m',
+    })
+  }
+
+  setGeoGuessrTimer = () => {
+    this.setState({
+      temporaryTime: '10m',
+    })
+  }
+
+  setRandomTimer = () => {
+    const min = 1;
+    const max = 60;
+    const timeInMinutes = Math.floor(Math.random() * (max - min + 1) + min);
+    const timeInSeconds = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(timeInMinutes, timeInSeconds);
+    this.setState({
+      temporaryTime: `${timeInMinutes}m ${timeInSeconds}s`,
     })
   }
 
   render() {
-    const { startCountdownDisabled, minutes, seconds, showTimer, showButtons } = this.state;
+    const { startCountdownDisabled, minutes, seconds, showTimer, temporaryTime } = this.state;
     const [minutesLeft, minutesRight] = String(minutes).padStart(2, '0');
     const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0');
     return(
       <div className="stopwatch-container">
+        <header className="stopwatch-header">
+          <span className="fixed-text">Work in Progress</span>
+        </header>
         <div className="stopwatch-timer">
           <div className="minutes">
             {showTimer &&
@@ -88,56 +157,62 @@ export default class StopWatch extends React.Component {
             </>
           }
         </div>
-        <>
-        <form className="form-container">
-          <div className="customInterval">
-            <input
-              type="text"
-              onChange={ this.handleInputTime }
-              placeholder="Ex.: 3m 25s"
-              />
-            <button
-              disabled={ startCountdownDisabled }
-              spellCheck={false}
-              onClick={ this.setStartCountdown }
-            >
-              {startCountdownDisabled ? <MdAlarmOff /> : <MdAlarmOn />}
-              Iniciar timer
+        { showTimer ? 
+          (
+            <button onClick={ this.cancelCountdown } className="cancelButton">
+              <MdAlarmOff />
+              Cancelar
             </button>
-          </div>
-          {showButtons ? 
-            (  
-            <div className="options">
-              <button
-                onClick={ this.handleFastTimer }
-              >
-                <BiTimer />
-                Vamos rápido
-              </button>
-              <button
-                onClick={ this.handleBreakTimer }
-                >
-                <MdLocalCafe />
-                Tempo para um café
-              </button>
-              <button
-                onClick={ this.handleGeoGuessrTimer }
-                >
-                <MdLocationOn />
-                GeoGuessr
-              </button>
-              <button
-                onClick={ this.handleRandomTimer }
-              >
-                <BiShuffle />
-                Aleatório
-              </button>
-            </div>) : null
-          }
-          
-        </form>
-        </>
-      </div>
+          ) : 
+          (
+            <>
+              <form className="form-container" onSubmit={ this.customInterval }>
+                <div className="customInterval">
+                  <input
+                    type="text"
+                    onChange={ this.handleInputTime }
+                    placeholder="Ex.: 3m 25s"
+                    value={ temporaryTime }
+                    />
+                  <button
+                    disabled={ startCountdownDisabled }
+                    spellCheck={false}
+                    >
+                    {startCountdownDisabled ? <MdAlarmOff /> : <MdAlarmOn />}
+                    Iniciar timer
+                  </button>
+                </div>
+                <div className="options">
+                  <button
+                    onClick={ this.setFastTimer }
+                  >
+                    <BiTimer />
+                    Vamos rápido
+                  </button>
+                  <button
+                    onClick={ this.setCoffeeTimer }
+                    >
+                    <MdLocalCafe />
+                    Tempo para um café
+                  </button>
+                  <button
+                    onClick={ this.setGeoGuessrTimer }
+                    >
+                    <MdLocationOn />
+                    GeoGuessr
+                  </button>
+                  <button
+                    onClick={ this.setRandomTimer }
+                    >
+                    <BiShuffle />
+                    Aleatório
+                  </button>
+                </div>
+              </form>
+            </>
+          )
+        };
+    </div>
     )
   }
 }
--- src/functions/time.js
+++ src/functions/time.js
@@ -4,3 +4,32 @@ export const validadeInputTime = (timeToValidade) => {
   return timeToValidade.match(timePattern);
 };
 
+export const convertTimeToMinutesAndSeconds = (time) => {
+  const minutes = String(Math.floor(time / 60)).padStart(2, '0');
+  const seconds = String(time % 60).padStart(2, '0');
+
+  return {
+    minutes,
+    seconds,
+  };
+};
+
+export const convertCustomTimeToSeconds = (temporaryTime) => {
+  temporaryTime = temporaryTime.split(/\s/);
+
+  let seconds = 0;
+
+  temporaryTime.forEach((unit) => {
+    if (unit.includes('m')) {
+      const temporaryMinutes = unit.replace('m', '');
+      const minutesInSeconds = Math.floor(temporaryMinutes * 60);
+      seconds += minutesInSeconds;
+    } else {
+      let temporarySeconds = unit.replace('s', '');
+      temporarySeconds = temporarySeconds % 60;
+      seconds += temporarySeconds;
+    }
+  })
+
+  return seconds;
+};
